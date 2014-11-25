9f2bb04810476dcf098359ba729ba839644ccc53
Albert Viscasillas <aviscasillas@gmail.com>
2014-11-25 08:52:59 +0100

    Jump to home screen

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,6 +3,17 @@ class AppDelegate < PM::Delegate
 
   def on_load(app, options)
     return true if RUBYMOTION_ENV == 'test'
-    open SignupScreen.new(nav_bar: false)
+
+    User.deserialize_from_file('user')
+
+    if current_user
+      open HomeScreen
+    else
+      open SignupScreen.new(nav_bar: false)
+    end
+  end
+
+  def current_user
+    @current_user = User.find { |user| !user.access_token.empty? }.first
   end
 end
---
+++ app/screens/home_screen.rb
@@ -0,0 +1,5 @@
+class HomeScreen < PM::Screen
+  title 'Send a wui'
+
+  tab_bar_item title: 'Send WUI', item: 'list'
+end
